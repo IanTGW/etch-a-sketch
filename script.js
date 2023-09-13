47e8dee6d03dcec02fee7fe896c67ffc7ff7c54c
IanTGW <ianwuxiinau@gmail.com>
2023-09-14 06:33:31 +1000

    Cap grid at 100x100 and add size selection with reset functionality

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
+let gridSize = Number(prompt("Choose your grid size", "16"));
+
+const gridSelector = document.querySelector('.gridSelector');
+gridSelector.addEventListener('click',chooseSize)
+
+
+
+function chooseSize () {
+    let gridSize = Number(prompt("Choose your grid size", "16"));
+    if(gridSize > 100){
+        gridSize = 100
+    };
+    
+    container.innerHTML = "";
+
+    for (let i = 0; i < gridSize; i++) {
+        box[i] = document.createElement('div');
+    };
+        
+    function printrow (rowElement) {
+        container.appendChild(rowElement);
+        rowElement.classList.add('rows')
+    };
+    
+    box.forEach(printrow);
+    
+    const rows = container.querySelectorAll('.rows');
+    
+    for(let row of rows){
+        for(let i = 0; i < gridSize; i++){
+            let column = document.createElement('div');
+            column.classList.add('grid');
+            row.appendChild(column);
+        };
+    };
+    
+    
+    const grid = container.querySelectorAll('.grid');
+    
+    function paintSquare (square) {
+    square.addEventListener('mouseover', ()=>square.style.backgroundColor = 'blue')
+    };
+    
+    grid.forEach(paintSquare)
+    
+};
 
 const container = document.querySelector('.container');
 
 let box = [];
 let column = [];
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < gridSize; i++) {
     box[i] = document.createElement('div');
 };
     
@@ -18,7 +64,7 @@ box.forEach(printrow);
 const rows = container.querySelectorAll('.rows');
 
 for(let row of rows){
-    for(let i = 0; i < 16; i++){
+    for(let i = 0; i < gridSize; i++){
         let column = document.createElement('div');
         column.classList.add('grid');
         row.appendChild(column);
